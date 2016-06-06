6381af4811066be18effc04f2e9c76e3529a7953
Connor Paull <connor.paull@gmail.com>
2016-06-06 16:55:57 -0700

    Added the python file for the repository

---
+++ bubbles.py
@@ -0,0 +1,26 @@
+
+rolls = input('Number of values: ')
+rollist = []
+for count in range (0, rolls):
+	import random
+	var = random.random()*10000
+	roundvar = round(var)
+	rollist.append(roundvar)
+sort = False  
+while not sort:
+	sort = True
+	for element in range(0, rolls-1):
+		if rollist[element] > rollist[element + 1]:
+			sort = False  # We found two elements in the wrong order
+			temp = rollist[element + 1]
+			rollist[element + 1] = rollist[element]
+			rollist[element] = temp
+print rollist
+
+
+
+
+
+
+
+
